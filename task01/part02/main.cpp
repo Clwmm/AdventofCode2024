49991144c757d05ee02dc32a47fb03113630a996
Clwmm <emgie4432@gmail.com>
2024-12-04 23:22:51 +0100

    task01 part 2

--- task01/part02/main.cpp
+++ task01/part02/main.cpp
@@ -1,19 +1,60 @@
-#include <immintrin.h>
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+
+std::string filename = "input/input";
+
+constexpr size_t MAP_SIZE = 90000;
+
+class HashMap
+{
+private:
+    int32_t tab[MAP_SIZE]{};
+
+    static size_t hash(const int32_t value) {
+        return value % MAP_SIZE;
+    }
+public:
+    void add(const int32_t value) {
+        tab[hash(value)]++;
+    }
+    int32_t get(const int32_t value) {
+        return tab[hash(value)];
+    }
+};
 
 int main() {
 
-    /* Initialize the two argument vectors */
-    __m256 evens = _mm256_set_ps(3.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0);
-    __m256 odds = _mm256_set_ps(3.0, 5.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0);
+    std::fstream inputFile(filename);
+    std::vector<int32_t> leftVec;
+    std::vector<int32_t> rightVec;
+    int64_t result = 0;
 
-    /* Compute the difference between the two vectors */
-    __m256 result = _mm256_sub_ps(evens, odds);
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        std::stringstream ss(line);
+        int x, y;
+        ss >> x >> y;
+        leftVec.push_back((int32_t)x);
+        rightVec.push_back((int32_t)y);
+    }
+    inputFile.close();
 
-    /* Display the elements of the result vector */
-    float* f = (float*)&result;
-    printf("%f %f %f %f %f %f %f %f\n",
-           f[0], f[1], f[2], f[3], f[4], f[5], f[6], f[7]);
+    HashMap rightMap;
+
+    for (size_t i = 0; i < rightVec.size(); ++i) {
+        rightMap.add(rightVec[i]);
+    }
+
+
+    for (auto x: leftVec) {
+        result += x * rightMap.get(x);
+    }
+
+
+    std::cout << "Result: " << result << std::endl;
 
     return 0;
 }
