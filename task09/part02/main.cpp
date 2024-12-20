7991488fdff920ada46e44c26f7b9ff3d3f5eb5e
Clwmm <emgie4432@gmail.com>
2024-12-21 00:25:50 +0100

    task09 part2

--- task09/part02/main.cpp
+++ task09/part02/main.cpp
@@ -3,13 +3,13 @@
 #include <string>
 #include <fstream>
 #include <vector>
-#include <set>
 
 std::string filename = "input/input";
 
 struct MemoryBlock {
     size_t startIdx;
     size_t size;
+    bool active = true;
 
     MemoryBlock(size_t startIdx, size_t size)
             : startIdx(startIdx), size(size) {}
@@ -25,8 +25,6 @@ struct FileBlock : public MemoryBlock {
 int main() {
     std::vector<FileBlock> files;
     std::vector<MemoryBlock> freeSpaces;
-    std::set<std::pair<size_t, size_t>> set;
-    std::set<std::pair<size_t, int64_t>> fullSet;
 
     std::fstream inputFile(filename);
     std::string line;
@@ -53,52 +51,16 @@ int main() {
     }
     inputFile.close();
 
-    for (const auto x : files) {
-        for (size_t i = x.startIdx; i < x.startIdx + x.size; ++i) {
-            fullSet.emplace(i, (int64_t)x.id);
-        }
-    }
 
-    for (const auto x : freeSpaces) {
-        for (size_t i = x.startIdx; i < x.startIdx + x.size; ++i) {
-            fullSet.emplace(i, -1);
-        }
-    }
-
-    for (const auto& x : fullSet) {
-        if (x.second == -1)
-            std::cout << ".";
-        else
-            std::cout << x.second;
-    }
-    std::cout << std::endl;
-
-//    while (true) {
-//        auto& freeSpace = freeSpaces[0];
-//        auto& file = files[files.size() - 1];
-//        if (freeSpace.startIdx + freeSpace.size > file.startIdx)
-//            break;
-//        const auto id = file.id;
-//        movedFiles.emplace_back(freeSpace.startIdx, 1, id);
-//        file.size--;
-//        if (file.size == 0)
-//            files.erase(files.end());
-//
-//        freeSpace.startIdx++;
-//        freeSpace.size--;
-//        if (freeSpace.size == 0)
-//            freeSpaces.erase(freeSpaces.begin());
-//    }
-
-    for (int64_t i = (int64_t)files.size() - 1; i >= 0; --i) {
+    for (int64_t i = static_cast<int64_t>(files.size()) - 1; i >= 0; --i) {
         auto& file = files[i];
 
-        for (auto it = freeSpaces.begin(); it != freeSpaces.end();) {
-            auto& freeSpace = *it;
-
-            if (freeSpace.size < file.size) {
-                ++it;
+        for (auto &freeSpace : freeSpaces) {
+            if (freeSpace.size < file.size || !freeSpace.active) {
                 continue;
+            }
+            if (file.startIdx < freeSpace.startIdx) {
+                break;
             }
 
             file.startIdx = freeSpace.startIdx;
@@ -106,13 +68,10 @@ int main() {
             freeSpace.startIdx += file.size;
 
             if (freeSpace.size == 0) {
-                it = freeSpaces.erase(it);
-            } else {
-                ++it;
+                freeSpace.active = false;
             }
             break;
         }
-
     }
 
     int64_t result = 0;
@@ -120,16 +79,8 @@ int main() {
     for (const auto x : files) {
         for (size_t i = x.startIdx; i < x.startIdx + x.size; ++i) {
             result += (int64_t)(i * x.id);
-            set.emplace(i, x.id);
         }
-    }
-
-    for (const auto& x : set) {
-        std::cout << x.first << " | " << x.second << std::endl;
     }
 
     std::cout << result << std::endl;
 }
-
-// 15834516286076 too high
-// 8564936405055 too high
